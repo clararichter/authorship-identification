64d59f825d52e7766c85f85aea2546d7ea830d37
clararichter <clara.l.richter@gmail.com>
2018-04-29 17:57:13 -0600

    hmmmm

--- spider.py
+++ spider.py
@@ -3,7 +3,8 @@ from urllib.request import urlopen, urlretrieve
 def spider():
     for i in range(2, 100):
         print("http://www.gutenberg.org/files/{}/{}-h/{}-h.htm".format(i, i, i))
-        urlretrieve( "http://www.gutenberg.org/files/{}/{}-h/{}-h.htm".format(i, i, i), "urls/book{}.html".format(i) )
+        urlretrieve( "http://www.gutenberg.org/files/{}/{}.txt".format(i, i), "urls/book{}.txt".format(i) )
 
 
+        #"http://www.gutenberg.org/files/{}/{}-h/{}-h.htm".format(i, i, i)
 spider()
