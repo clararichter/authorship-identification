0f87a750e0f7581e20fc2cccff99bbe47c530c79
sunnychiu312 <sunny.chiu@coloradocollege.edu>
2018-04-30 02:18:09 -0600

    update todos

--- book.py
+++ book.py
@@ -7,12 +7,12 @@ url ="https://www.gutenberg.org/dirs/GUTINDEX."
 
 books = dict()
 
-for year in range(1996, 2019):
+startYear = 1996
+endYearPlus1 = 1997
+for year in range(startYear, endYearPlus1):
     print(year)
     page = requests.get(url + str(year))
     soup = BeautifulSoup(page.content, 'html.parser')
-
-
 
     w = []
     for string in soup.strings:
@@ -22,27 +22,31 @@ for year in range(1996, 2019):
     for i in range(1,len(rows)):
 
         comma = True
-        row = rows[i].split("by")
+        row = rows[i].split("by") #TODO: mutiple "by"'s cause incorrect split
         if len(row) == 1:
             row = rows[i].split(",")
             comma = False
+
         title = row[0].strip()
+
         if comma:
             title = title[:-1]
-        #print(i, " ",row)
 
         if len(row) == 1:
-            continue#no author
+            continue #no author
+
         subtitle = row[1].split("\\r\\n")
-        #author = " ".join(subtitle[0].split())
         info = subtitle[0].split()
-        if len(info) == 0:
-            continue #empty
+
+        if len(info) == 0: #empty, I don't know why this can happen
+            continue
+
         #print(info)
         no = info[len(info)-1]
         try:
-            int(no)
+            int(no) #check if there is no.
             author = info[:len(info)-1]
+            #TODO: if title is too long, no. is in between of title and author
         except ValueError:
             author = info
             no = "NaN"
@@ -50,7 +54,7 @@ for year in range(1996, 2019):
         if "and" in author:
             index = author.index("and")
             a1 = " ".join(author[:index])
-            a2 = " ".join(author[index +1 :])
+            a2 = " ".join(author[index +1 :]) #TODO: change for multiple authors
             author = (a1, a2)
         else:
             author = " ".join(author)
