6a265ca17e1964ea8f4d47472a7b7b65430e2e79
Beau Carlborg <bcarlborg@gmail.com>
2018-04-30 20:23:40 -0600

    unify the scrapers

--- goodreads_list_link_generator.py
+++ goodreads_list_link_generator.py
@@ -1,6 +1,31 @@
 import requests, lxml.html
 from bs4 import BeautifulSoup
+import csv
+import re
 
+def publish_info(url):
+  page = requests.get(url)
+  soup = BeautifulSoup(page.content, 'html.parser')
+
+  title_box = soup.find("h1", class_ = "bookTitle" )
+  title = title_box.contents[0].strip()
+
+  authors_box = soup.find_all("span", itemprop ="name")
+  author = []
+  for a in authors_box:
+    author += a.contents
+
+  date_box = soup.find_all("div", class_="row", limit = 2)
+  pub = date_box[1].find("nobr", class_ = "greyText")
+  if pub == None:
+    pub = date_box[1].contents[0]
+  pub = list(map(int, re.findall(r'\d+', pub.string)))
+  if len(pub) == 1:
+    date =  pub[0]
+  else:
+    date = pub[1]
+
+  return {"title": title, "author": author, "date": date}
 
 s = requests.session()
 login = s.get("https://www.goodreads.com/user/sign_in")
@@ -14,7 +39,7 @@ response = s.post("https://www.goodreads.com/user/sign_in", data=form)
 print("response: url: ", response.url)
 print("beau in response", ("beau" in response.text))
 
-list_2 = s.get("https://www.goodreads.com/shelf/show/project-gutenberg?page=2")
+list_2 = s.get("https://www.goodreads.com/shelf/show/project-gutenberg?page=1")
 # print(list_2.text)
 soup = BeautifulSoup(list_2.text, 'html.parser')
 link_list = soup.find_all("a", class_="bookTitle", href=True)
@@ -22,6 +47,4 @@ link_list = soup.find_all("a", class_="bookTitle", href=True)
 base_url = "https://www.goodreads.com"
 url_list = []
 for book in link_list:
-  url_list.append(base_url + book['href'])
-
-print(url_list)
+  print(publish_info(base_url + book['href']))
