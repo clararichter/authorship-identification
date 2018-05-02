ad6681393ad03fee61059f71d94db08d3f64c2f9
clararichter <clara.l.richter@gmail.com>
2018-05-01 18:54:30 -0600

    parse...

--- enhanced_parser.py
+++ enhanced_parser.py
@@ -1,29 +1,43 @@
 import csv
 import re
 
-outfile = open('./gutenberg_books.csv','w')
-writer = csv.writer(outfile, delimiter=' ', quotechar='\t', quoting=csv.QUOTE_MINIMAL)
+#outfile = open('./gutenberg_books.csv','w')
+#writer = csv.DictWriter(outfile, delimiter=' ', quotechar='\t', quoting=csv.QUOTE_MINIMAL)
 
 
-#text = '''\nLucian the dreamer, by J. S. Fletcher                                    55484'''
-content = open("catalog.txt").read()
+content = open("head.txt").read()
 content = re.sub(' +',' ', content)
 content = content.replace(u'\xa0', u' ')
-#print(content)
-pattern = r'\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+ [0-9]+'
 
-works = re.findall(pattern, content)
+pattern1 = r'\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+ [0-9]+'
+pattern2 = r'\n[A-Za-z0-9 ,.\'()-:]+ [0-9]+\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+'
 
-test = '''
-On the Supply of Printed Books from the Library to the Reading 39087
- Room of the British Museum, by Anthony Panizzi
-'''
+works = re.findall(pattern1, content)
+# preprocess works
 
-pattern = r'\n[A-Za-z0-9 ,.\'()-:]+ [0-9]+\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+'
-works2 = re.findall(pattern, content)
+for w in works:
+    #print(w)
+    title = re.search('.+(?=, by)', w)
+    author_and_id = re.search('(?<=, by ).+', w)
+    author = re.search('.+(?= [0-9]+)', author_and_id[0])
+    book_id = re.search('[0-9]+', author_and_id[0])
 
-#
-# #print(content)
-# for w in works:
-#     print(w)
-print(len(works) + len(works2))
+    print("TITLE: ", title[0])
+    print("AUTHOR: ", author[0])
+    print("ID: ", book_id[0])
+    #writer.writerow( {"title": title[0], "author": author[0], "book_id" : book_id[0]} )
+
+works = re.findall(pattern2, content)
+# preprocess works
+
+for w in works:
+    #print(w)
+    title = re.search('.+(?=, by)', w)
+    author_and_id = re.search('(?<=, by ).+', w)
+    author = re.search('.+(?= [0-9]+)', author_and_id[0])
+    book_id = re.search('[0-9]+', author_and_id[0])
+
+    print("TITLE: ", title[0])
+    print("AUTHOR: ", author[0])
+    print("ID: ", id[0])
+    #writer.writerow( {"title": title[0], "author": author[0], "book_id" : book_id[0]} )
