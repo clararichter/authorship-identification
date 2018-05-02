0c5c0d1255a93a51f2b7f642716d7e928e3df3d3
clararichter <clara.l.richter@gmail.com>
2018-05-01 22:43:37 -0600

    clean up

---
+++ gutenberg_catalog_parser.py
@@ -0,0 +1,44 @@
+import csv
+import re
+
+outfile = open('./gutenberg_catalog.csv','w')
+writer = csv.DictWriter(outfile, fieldnames=['title', 'author', 'book_id'])
+
+
+content = open("gutenberg_original_catalog.txt").read()
+content = re.sub(' +',' ', content)
+content = content.replace(u'\xa0', u' ')
+
+pattern1 = r'\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+ [0-9]+'
+pattern2 = r'\n[A-Za-z0-9 ,.\'()-:]+ [0-9]+\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+'
+
+
+works = re.findall(pattern1, content)
+for w in works:
+    print(w)
+    title = re.search('.+(?=, by)', w)
+    author_and_id = re.search('(?<=, by ).+', w)
+    author = re.search('.+(?= [0-9]+)', author_and_id[0])
+    book_id = re.search('[0-9]+', author_and_id[0])
+
+    print("TITLE: ", title[0])
+    print("AUTHOR: ", author[0])
+    print("ID: ", book_id[0])
+    writer.writerow( {"title": title[0], "author": author[0], "book_id" : book_id[0]} )
+
+content = re.sub(pattern1, '', content)
+
+works = re.findall(pattern2, content)
+for w in works:
+    print(w)
+    w = w.replace('\n', ' ')
+    author = re.search('(?<=, by ).+', w)
+
+    title_and_id = re.search('.+(?=, by)', w)
+    book_id = re.search( r'( (\d+) )', title_and_id[0] )
+    #print(title_and_id[0])
+    title = re.sub(r' (\d+) ', '', title_and_id[0])
+    print("TITLE: ", title)
+    print("AUTHOR: ", author[0])
+    print("ID: ", book_id[0])
+    writer.writerow( {"title": title, "author": author[0], "book_id" : book_id[0]} )
