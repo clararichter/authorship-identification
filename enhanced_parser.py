c529782a85fd0774bc8d4b18d70ea828663d4c8f
clararichter <clara.l.richter@gmail.com>
2018-05-01 19:18:10 -0600

    parse...

--- enhanced_parser.py
+++ enhanced_parser.py
@@ -13,10 +13,8 @@ pattern1 = r'\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+ [0-9]+'
 pattern2 = r'\n[A-Za-z0-9 ,.\'()-:]+ [0-9]+\n[A-Za-z0-9 ,.\'()-:]+, by [A-Za-z .-]+'
 
 works = re.findall(pattern1, content)
-# preprocess works
-
 for w in works:
-    #print(w)
+    print(w)
     title = re.search('.+(?=, by)', w)
     author_and_id = re.search('(?<=, by ).+', w)
     author = re.search('.+(?= [0-9]+)', author_and_id[0])
@@ -28,16 +26,16 @@ for w in works:
     #writer.writerow( {"title": title[0], "author": author[0], "book_id" : book_id[0]} )
 
 works = re.findall(pattern2, content)
-# preprocess works
-
 for w in works:
-    #print(w)
-    title = re.search('.+(?=, by)', w)
-    author_and_id = re.search('(?<=, by ).+', w)
-    author = re.search('.+(?= [0-9]+)', author_and_id[0])
-    book_id = re.search('[0-9]+', author_and_id[0])
+    print(w)
+    w = w.replace('\n', ' ')
+    author = re.search('(?<=, by ).+', w)
 
+    title_and_id = re.search('.+(?=, by)', w)
+    book_id = re.search( r'( (\d+) )', title_and_id[0] )
+    #print(title_and_id[0])
+    title = re.sub( r'( (\d+) )', 'BLEH', title_and_id[0] )
     print("TITLE: ", title[0])
     print("AUTHOR: ", author[0])
-    print("ID: ", id[0])
-    #writer.writerow( {"title": title[0], "author": author[0], "book_id" : book_id[0]} )
+    print("ID: ", book_id[0])
+    # #writer.writerow( {"title": title[0], "author": author[0], "book_id" : book_id[0]} )
