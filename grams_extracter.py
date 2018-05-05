53e5b434eda9a4ce591c0e7fe0f35061cb0fba0e
clararichter <clara.l.richter@gmail.com>
2018-05-05 14:47:32 -0600

    confused

--- grams_extracter.py
+++ grams_extracter.py
@@ -3,14 +3,19 @@ from nltk.util import ngrams
 from nltk import word_tokenize
 import operator
 
+
 def get_vector(text):
     text = text.lower()
     tokens = nltk.word_tokenize(text)
+
     map =  {}
-    for n in range(2, 3):
+    for n in range(1, 7):
         map.update( get_ngram_vector(tokens, n) )
     return map
 
+def get_toks():
+    pos_tags = [ pos for ( word, pos ) nltk.pos_tag(tokens) ]
+    print pos_tags
 
 def get_ngram_vector(tokens, n):
     grams = ngrams(tokens, n)
@@ -18,6 +23,12 @@ def get_ngram_vector(tokens, n):
     for ( gram, count ) in gram_map.items():
         gram_map[gram] = ( count, count / total_number_of_grams )
     return gram_map
+
+def get_n_tok_vector(pos_tags, n):
+
+    for l in range(len(pos_tags)):
+        for i in range(1, len(pos_tags)-l):
+            post_tags[i:i+l]
 
 
 def get_gram_map(grams):
