06a95d9f342d1b545e3926b804baf434cdc90add
clararichter <clara.l.richter@gmail.com>
2018-05-06 20:53:41 -0600

    i hate regex

--- data/scripts/text_retrieval.py
+++ data/scripts/text_retrieval.py
@@ -1,10 +1,15 @@
 from urllib.request import urlretrieve
-import json
-import os
+import csv
 
-for file in os.listdir("../links"):
-    print(file)
-    with open('../links/' + file) as f:
-        data = json.load(f)
-        for book_object in data:
-            urlretrieve( book_object['link'], "../texts_raw/{}/{}.txt".format(book_object["author"].split()[1].lower(), book_object["title"].replace(" ", "_").lower()  ) )
+with open('books.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    for book in reader:
+        print(book['author'])
+
+#
+# for file in os.listdir("../links"):
+#     print(file)
+#     with open('../links/' + file) as f:
+#         data = json.load(f)
+#         for book_object in data:
+#             urlretrieve( book_object['link'], "../texts_raw/{}/{}.txt".format(book_object["author"].split()[1].lower(), book_object["title"].replace(" ", "_").lower()  ) )
