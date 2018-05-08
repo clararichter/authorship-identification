02096fae95cdf31cbd54604d7b46adf970ec3828
sunnychiu312 <sunny.chiu@coloradocollege.edu>
2018-05-07 21:45:35 -0600

    new extrater

--- grams_extracter.py
+++ grams_extracter.py
@@ -1,33 +1,28 @@
-import nltk
 from nltk.util import ngrams
 #from nltk import word_tokenize
 from nltk.tokenize import TweetTokenizer, sent_tokenize
 import pandas as pd
 import numpy as np
+
 class Text():
 
     def __init__(self, text):
         self.text = text.lower()
-        #self.tokens = TweetTokenizer().tokenize(self.text)
-        #self.pos_tags = nltk.pos_tag(self.tokens)
-        #self.total_number_of_tokens = len(self.tokens)
+        self.tokens = TweetTokenizer().tokenize(self.text)
+        self.sentences = sent_tokenize(self.text)
+        self.total_number_of_tokens = len(self.tokens)
         self.row = {}
-        self.build_row()
-
-        self.sent_len_dis = {}
-        self.word_len_dis = {}
-
+        #self.build_row()
 
     def build_row(self):
-        #self.retrieve_gram_data()
-        #self.retrieve_pos_data()
-        self.retrieve_avg_sentence_word()
-        # self.retrieve_word_diversity_data()
-        # self.retrieve_word_length_data()
-        # self.retrieve_sentence_length_data()
+        self.gram_data()
+        self.pos_data()
+        self.word_richness()
+        self.word_length_data()
+        self.sentence_length_data()
 
 
-    def retrieve_gram_data(self):
+    def gram_data(self):
         for n in range(1, 3):
             n_grams = ngrams(self.tokens, n)
             number_of_n_grams = self.total_number_of_tokens - n + 1
@@ -42,92 +37,48 @@ class Text():
                 count_map[item] = 1
         return count_map
 
-    # def percentage_map(self, count_map, total):
-    #     return( { key : (lambda count: count / total)(count) for ( key, count ) in counter_map.items() } )
+    def percentage_map(self, count_map, total):
+        return( { key : (lambda count: count / total)(count) for ( key, count ) in count_map.items() } )
 
 
-    # def get_word_length_distr(self):
-    #     return self.percentage_map( self.count_map( [ len(word) for word in self.tokens ] ), self.total_number_of_tokens )
-
-
-    def retrieve_pos_data(self):
+    def pos_data(self):
+        pos_tags = nltk.pos_tag(self.tokens)
         for n in range(1, 3):
             pos_n_grams = []
-            for i in range(0, len(self.pos_tags)+1-n):
-                pos_n_grams.append( tuple([ tag for (word, tag) in  self.pos_tags[i:i+n]]) )
+            for i in range(0, len(pos_tags)+1-n):
+                pos_n_grams.append( tuple([ tag for (word, tag) in  pos_tags[i:i+n]]) )
             self.row.update( {  " ".join(pos_n_gram) : (lambda count: count / len(pos_n_grams))(count) for ( pos_n_gram, count ) in self.count_map(pos_n_grams).items() } )
 
 
+    def word_lengths(self):
+        word_lengths = []
+        forbidden = [".", ",", "?", "!", "\"","\'",";",":"]
+        for word in self.tokens:
+            if word not in forbidden:
+                word_lengths.append(len(word))
+        return sorted(word_lengths)
 
-    def retrieve_avg_sentence_word(self):
-        sent_tokenize_list = sent_tokenize(self.text)
-        #length count map
-        sent_length = []
-        word_length = []
-        for sent in sent_tokenize_list:
-            count = 0
-            for word in TweetTokenizer().tokenize(sent):
-                punt = [".", ",", "?", "!", "\"","\'",";",":"]
-                if word not in punt:
-                    count += 1
-                    word_length.append(len(word))
-            sent_length.append(count)
-        self.sent_len_dis = self.count_map(sorted(sent_length))
-        self.word_len_dis = self.count_map(sorted(word_length))
+    def sentence_lengths(self):
+        sentence_lengths = []
+        for sentence in self.sentences:
+            sentence_lengths.append(len(sentence.split(" ")))
+        return sorted(sentence_lengths)
 
-        avg_sent_length = np.mean(sent_length)
-        avg_word_length = np.mean(word_length)
-        # return (avg_sent_length, avg_word_length)
-        self.row.update({ "avg_sent_length": avg_sent_length, "avg_word_length": avg_word_length})
-        print (float("%.2f" % avg_sent_length), float("%.2f" % avg_word_length))
+    def word_length_distr(self):
+        word_lengths = self.word_lengths()
+        return self.percentage_map( self.count_map(word_lengths), len(word_lengths) )
 
+    def sentence_lengths_distr(self):
+        sentence_lengths = self.sentence_lengths()
+        return percentage_map( count_map(sentence_lengths), len(sentence_lengths) )
 
-def main():
-    matrix = pd.DataFrame()
+    def word_length_data(self):
+        word_lengths = self.word_lengths()
+        self.row.update( {'word_length_avg': np.mean(word_lengths),'word_length_std_dev': np.std(word_lengths)} )
 
-    para1 = """
-        Marley was dead, to begin with. There is no doubt whatever about that.
-        The register of his burial was signed by the clergyman, the clerk, the
-        undertaker, and the chief mourner. Scrooge signed it. And Scrooge's name
-        was good upon 'Change for anything he chose to put his hand to. Old
-        Marley was as dead as a door-nail.
-    """
-    para2 = """
-        Scrooge knew he was dead? Of course he did. How could it be otherwise?
-        Scrooge and he were partners for I don't know how many years. Scrooge
-        was his sole executor, his sole administrator, his sole assign, his sole
-        residuary legatee, his sole friend, and sole mourner. And even Scrooge
-        was not so dreadfully cut up by the sad event but that he was an
-        excellent man of business on the very day of the funeral, and solemnised
-        it with an undoubted bargain.
-    """
-    books_df = pd.DataFrame()
-    paras = [para1, para2]
-    for i in range(0, len(paras)):
-        print(i)
-        text = Text(paras[i])
-        index = "para"+ str(i)
-        book_df = pd.DataFrame(text.row,index =[index])
-        books_df = books_df.append(book_df)
-    with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
-        print(books_df)
+    def sentence_length_data(self):
+        sentence_lengths = self.sentence_lengths()
+        self.row.update( {'sentence_length_avg': np.mean(sentence_lengths), 'sentence_length_std_dev': np.std(sentence_lengths) })
 
-    # books = ["a_bullet_for_cinderella_49931", "a_child's_dream_of_a_star_42232"]
-    # #books = ["a_bullet_for_cinderella_49931"]
-    #
-    # base = "ice_box/publish_date_books/books/"
-    # file_type = ".txt"
-    # for book in books:
-    #     dic = base + book + file_type
-    #     file = open(dic,'r')
-    #     text = Text(file.read())
-    #     #text = Text(" the beautiful cat sits. the beautiful cat sits ")
-    #     text.build_row()
-    #     book_df = pd.DataFrame(text.row,index =[book])
-    #     matrix = matrix.append(book_df)
-    # #with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
-    #     #print(matrix)
-
-
-if __name__ == '__main__':
-    main()
+    def word_richness(self):
+        self.row.update( {'word_richness': len(self.tokens) / len(set(self.tokens)) } )
