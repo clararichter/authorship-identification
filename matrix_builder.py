c3492d304b8295b87c168b195f3de3121a2fbd9a
Beau Carlborg <bcarlborg@gmail.com>
2018-05-14 17:08:32 -0600

    new matrix builder

--- matrix_builder.py
+++ matrix_builder.py
@@ -7,7 +7,7 @@ import numpy as np
 class Matrix():
 
     def __init__(self, authors):
-        path = "data/texts_cleaned/"
+        path = "data/texts_cleaned"
         self.matrix = pd.DataFrame()
         self.authors = authors
         self.build_matrix(path)
@@ -15,25 +15,34 @@ class Matrix():
 
     def build_matrix(self, path):
         for author in self.authors:
+            print(author)
             for book in os.listdir(path + '/' + author):
+                print("\t", book)
                 text = Text( open(path + '/' + author + '/' + book, 'r', errors='ignore').read(), author )
                 self.matrix = self.matrix.append( pd.DataFrame(text.get_vector(), index=[book] ) )
 
+        print("features retrieved")
         self.matrix = self.matrix.fillna(0)
+        # self.matrix.to_csv("matrix_builder_full_table.csv", encoding='utf-8')
         self.eliminate_sparse_columns(.5)
+        self.matrix.to_csv("matrix_builder_post_sparcity.csv", encoding='utf-8')
         self.eliminate_non_significant_columns(.3)
-        self.matrix.to_csv("booksyay.csv", encoding='utf-8')
+        self.matrix.to_csv("matrix_builder_post_significance.csv", encoding='utf-8')
 
 
 
     def eliminate_sparse_columns(self, threshold): # threshold -> min_percent_data
+        # print("starting csv read")
+        # self.matrix = pd.read_csv("./matrix_builder_full_table.csv", header=0, index_col=0)
 
-        columns_to_keep = ['author', 'word_length_avg', 'word_length_std_dev', 'sentence_length_avg', 'sentence_length_std_dev', 'word_richness']
+        print("eliminate_sparce_columns_function")
+        columns_to_keep = ['original_book_author', 'word_length_avg', 'word_length_std_dev', 'sentence_length_avg', 'sentence_length_std_dev', 'word_richness']
         feature_list = list( set(self.matrix.columns.values).difference( set( columns_to_keep ) ) )
         sparcity_data = pd.DataFrame(columns=self.authors, index=feature_list)
 
         for author in self.authors:
-            subframe = self.matrix.loc[self.matrix['author'] == author]
+            print("\t", author)
+            subframe = self.matrix.loc[self.matrix['original_book_author'] == author]
             for column in subframe:
                 if column not in columns_to_keep:
                     subcolumn = subframe[column]
@@ -48,9 +57,12 @@ class Matrix():
 
 
     def eliminate_non_significant_columns(self, threshold):
-        print('eliminate signifcant')
+        # print("starting csv read")
+        # self.matrix = pd.read_csv("./matrix_builder_post_sparcity.csv", header=0, index_col=0)
+
+        print('eliminate_non_significant_columns function')
         # Columns we want to avoid
-        columns_to_keep = ['author', 'word_length_avg', 'word_length_std_dev', 'sentence_length_avg', 'sentence_length_std_dev', 'word_richness']
+        columns_to_keep = ['original_book_author', 'word_length_avg', 'word_length_std_dev', 'sentence_length_avg', 'sentence_length_std_dev', 'word_richness']
         # Make two tables for the means and std's by authors
         feature_list = list( set(self.matrix.columns.values).difference( set( columns_to_keep ) ) )
         mean_feature_data = pd.DataFrame(columns=self.authors, index=feature_list)
@@ -59,23 +71,23 @@ class Matrix():
         feature_data = pd.DataFrame(columns=['mean_of_stds','std_of_means'], index=feature_list)
 
         for author in self.authors:
-            print('jane autsten')
-            subframe = self.matrix.loc[self.matrix['author'] == author]
+            print("\t", author)
+            subframe = self.matrix.loc[self.matrix['original_book_author'] == author]
             for column in subframe:
                 if column not in columns_to_keep:
                     mean_feature_data.at[column, author] = np.mean(subframe[column])
                     std_feature_data.at[column, author] = np.std(subframe[column])
 
+        print("\tsub tables built, constructing feature table")
         for column in feature_list:
-            print(column)
             feature_data.at[column,'mean_of_stds'] = np.mean(std_feature_data.loc[column])
             feature_data.at[column,'std_of_means'] = np.mean(mean_feature_data.loc[column])
 
-
+        print("\tdrops")
         drops = feature_data.sort_values(by=['mean_of_stds']).head( int(threshold * feature_data.shape[0]))
-        self.matrix = self.matrix.drop( feature_data.index, axis=1 )
+        self.matrix = self.matrix.drop( drops.index, axis=1 )
         drops = feature_data.sort_values(by=['std_of_means']).tail( int(threshold * feature_data.shape[1]))
-        self.matrix = self.matrix.drop( feature_data.index, axis=1 )
+        self.matrix = self.matrix.drop( drops.index, axis=1 )
 
 if __name__ == '__main__':
-    matrix = Matrix(['jane_austen'])
+    matrix = Matrix(['oscar_wilde', 'mildred_a._wirt'])
