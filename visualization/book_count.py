3b44088ce272e64c6c589e3d3f0688ffaf6573fa
clararichter <clara.l.richter@gmail.com>
2018-05-15 12:39:14 -0600

    cleanup

--- visualization/book_count.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_book_count_data():
-    book_counts = []
-    path = "../data/texts_stripped/"
-    for author_folder in os.listdir(path):
-        book_counts.append(len(os.listdir(path + author_folder)))
-    return ( os.listdir(path), book_counts )
